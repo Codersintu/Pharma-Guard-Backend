267fdd50ad03fba772b45f6aa5f349113eeb247a
Sintu Mahto <skmahto9199@gmail.com>
2026-02-20 05:55:10 +0530

    fixed bug

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,8 @@ app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
   })
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-  });
-}
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
